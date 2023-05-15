d8836463621722171ae8c4a5595ca3de5e5e6e19
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-05-15 12:55:37 +0300

    feat: get all parents

--- main.js
+++ main.js
@@ -42,6 +42,20 @@ class TreeStore {
 
 		return [...children, ...getChildrenNextLevel(children)];
 	}
+
+	getAllParents(id) {
+		let item = this.getItem(id);
+
+		const allParents = [];
+		while (item.parent !== "root") {
+			const parentItem = this.getItem(item.parent);
+
+			allParents.push(parentItem);
+			item = parentItem;
+		}
+
+		return allParents;
+	}
 }
 
 const items = [
@@ -70,3 +84,5 @@ console.log(ts.getChildren(5), "\n"); // []
 console.log(ts.getChildren(2), "\n"); // [{"id":4,"parent":2,"type":"test"},{"id":5,"parent":2,"type":"test"},{"id":6,"parent":2,"type":"test"}]
 
 console.log(ts.getAllChildren(2), "\n"); // [{"id":4,"parent":2,"type":"test"},{"id":5,"parent":2,"type":"test"},{"id":6,"parent":2,"type":"test"},{"id":7,"parent":4,"type":null},{"id":8,"parent":4,"type":null}]
+
+console.log(ts.getAllParents(7)); // [{"id":4,"parent":2,"type":"test"},{"id":2,"parent":1,"type":"test"},{"id":1,"parent":"root"}])
