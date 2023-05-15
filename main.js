0c41851700d36f0b5efda3b7111eb4d3b426afa8
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-05-15 11:37:13 +0300

    feat: get children for parent

--- main.js
+++ main.js
@@ -14,6 +14,13 @@ class TreeStore {
 	getItem(id) {
 		return this.items.get(id);
 	}
+
+	getChildren(id) {
+		const parent = this.getItem(id);
+		const items = this.getAll();
+
+		return items.filter(item => item.parent == parent.id);
+	}
 }
 
 const items = [
@@ -31,6 +38,8 @@ const items = [
 
 const ts = new TreeStore(items);
 
-console.log(ts.getAll(), '\n');
+console.log(ts.getAll(), "\n");
 
-console.log(ts.getItem(7), '\n'); // {"id":7,"parent":4,"type":null}
+console.log(ts.getItem(7), "\n"); // {"id":7,"parent":4,"type":null}
+
+console.log(ts.getChildren(4), "\n"); // [{"id":7,"parent":4,"type":null},{"id":8,"parent":4,"type":null}])
